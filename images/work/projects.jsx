fd8748c87627ec354221cc30d6331e6a67e7d6d0
sarahatesyou <sarafcso@gmail.com>
2026-06-13 11:37:48 +0100

    fix bugs, add new project images and update about me section

---
+++ images/work/projects.jsx
@@ -0,0 +1,39 @@
+import cookbook from "./cookbook.png";
+import clock from "./clock.png";
+import weather from "./weather.png";
+import plane from "./plane.png";
+
+export const projects = [
+  {
+    href: "https://sadies-ai-recipe-generator.netlify.app/",
+    img: cookbook,
+    alt: "recipe app logo",
+    title: "Recipe app",
+    description:
+      "A website that uses AI to create vegetarian recipes with a given ingredient.",
+  },
+  {
+    href: "https://sadies-world-clock.netlify.app/",
+    img: clock,
+    alt: "world clock app logo",
+    title: "World Clock app",
+    description:
+      "A website that displays the current time on several cities using an API.",
+  },
+  {
+    href: "https://main--sadiesweatherapp.netlify.app/",
+    img: weather,
+    alt: "weather app logo",
+    title: "Weather app",
+    description:
+      "A website that provides current weather conditions for a given city using an API.",
+  },
+  {
+    href: "/vacation.html",
+    img: plane,
+    alt: "unforgettable vacation challenge app logo",
+    title: "SheCodes Challenge Entry",
+    description:
+      "My entry for the Unforgettable Vacation Challenge on SheCodes. I placed 3rd.",
+  },
+];
--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,41 +1,7 @@
 import "./style.css";
 import Sticker from "./Sticker";
 import { stickers } from "../images/stickers/stickers.jsx";
-
-const projects = [
-  {
-    href: "https://sadies-ai-recipe-generator.netlify.app/",
-    img: "/images/work/eat.png",
-    alt: "recipe app logo",
-    title: "Recipe app",
-    description:
-      "A website that uses AI to create vegetarian recipes allows users to input a specific ingredient, then generates customized recipes featuring that ingredient. Built with HTML, CSS and JavaScript",
-  },
-  {
-    href: "https://sadies-world-clock.netlify.app/",
-    img: "/images/work/world-clock.png",
-    alt: "world clock app logo",
-    title: "World Clock app",
-    description:
-      "A website that displays the current time in various cities around the world, as well as the local time based on the user's current location. Built with HTML, CSS and JavaScript",
-  },
-  {
-    href: "https://main--sadiesweatherapp.netlify.app/",
-    img: "/images/work/weather.png",
-    alt: "weather app logo",
-    title: "Weather app",
-    description:
-      "A website that provides current weather conditions and a weekly forecast for a city specified by the user, offering up-to-date and accurate meteorological information. Built with HTML, CSS and JavaScript",
-  },
-  {
-    href: "https://www.shecodes.io/contests/unforgettable-vacation-challenge/contest_entries/sara-cardinal-portuguese/preview",
-    img: "/images/work/austria.png",
-    alt: "unforgettable vacation challenge app logo",
-    title: null,
-    description:
-      'My entry for the Unforgettable Vacation Challenge on SheCodes. The prompt was "Create a site showcasing your favorite vacation moment." and I placed 4th.',
-  },
-];
+import { projects } from "../images/work/projects.jsx";
 
 function SideTitle({ href, children }) {
   return (
@@ -113,45 +79,44 @@ export default function SaraPortfolio() {
       <section className="section about" id="about">
         <div className="main-content">
           <p>
-            My passion for web coding <strong>began when I was 13</strong>,
-            hand-crafting my MySpace page with HTML and CSS to make it uniquely
-            mine (a true millennial rite of passage). I{" "}
-            <strong>continued learning on my own</strong>, creating fan pages
-            for my favourite musicians and experimenting with design.
+            coding has been one of my passions since i was 13 years old. like a
+            true millennial,{" "}
+            <strong>my first coding experience was on myspace</strong>. but my
+            first professional experience started when my friends would ask me
+            to create fan blogs for our favourite musicians.{" "}
+            <strong>that’s how i taught myself html and css</strong>.
             <br />
             <br />
-            Later, I pursued a{" "}
+            <strong>writing code was such a fun hobby</strong> that only when a
+            family friend told me — a very lost teenager — “choose a job that
+            won't make you dread waking up on monday mornings” did it click.{" "}
+            <strong>i had to become a professional web developer</strong>.
+            <br />
+            <br />i took a{" "}
             <strong>
-              Higher Technical Professional Course in Information Systems
-              Technologies and Programming at Universidade Europeia
+              technological specialisation course in information systems
+              programming
             </strong>
-            , where my interest in web development solidified.
+            , and by far my favourite subjects were those related to web
+            programming.
             <br />
             <br />
-            Life then took me in a different direction, and I{" "}
+            <i>mick jagger once said “you can’t always get what you want”</i>,
+            and life took me in a different path.{" "}
             <strong>
-              built a career in social media management and content creation
+              i worked as a social media manager / content creator for 10 years
             </strong>
-            . Over seven years, I developed strong skills in digital
-            communication, creativity, and adaptability.
+            . and as much as it was a fun and creative job, something was
+            missing. so{" "}
+            <strong>i went back to coding, and took a shecodes workshop</strong>
+            . and the most fun i had at my job was working on my portfolio
+            during lunch breaks.
             <br />
             <br />
-            <strong>Recently, I decided to return to my original plan</strong>.
-            After completing the <strong>SheCodes Front-End Course</strong>, I
-            am now ready to transition into front-end development: combining my
-            technical foundation with years of experience in storytelling and
-            digital strategy. My core skills lie in{" "}
-            <strong>building responsive web applications</strong> with a focus
-            on <strong>clean</strong>,{" "}
-            <strong>minimalist design principles</strong>. I strive to create
-            web experiences that are not only aesthetically pleasing but also
-            highly functional and intuitive for the user.
-            <br />
-            <br />
-            Outside of work, you will often find me relaxing with my two dogs,
-            enjoying live music at concerts, or watching a football match. I
-            also have a soft spot for all things Halloween, which often fuels my
-            sense of style and imagination.
+            <i>
+              <strong>well, that's my life!</strong> thank you so much for
+              spending time with me. i hope you enjoyed it because i know i did.
+            </i>
           </p>
         </div>
         <SideTitle href="#work">my work</SideTitle>
@@ -165,7 +130,9 @@ export default function SaraPortfolio() {
               <>
                 <div className="item" key={`img-${i}`}>
                   <a href={project.href} target="_blank" rel="noreferrer">
-                    <img src={project.img} alt={project.alt} />
+                    <div className="sticker-wrapper">
+                      <img src={project.img} alt={project.alt} />
+                    </div>
                   </a>
                 </div>
                 <div className="item" key={`desc-${i}`}>
@@ -192,7 +159,7 @@ export default function SaraPortfolio() {
       <section className="section contact" id="contact">
         <div className="main-content">
           <div className="content-block">
-            <p>📧 hello@sarahatesyou.dev</p>
+            <p>📧 hello@sarahatesyou.pt</p>
             <div className="social">
               <a
                 href="https://www.linkedin.com/in/sarahatesyou"
